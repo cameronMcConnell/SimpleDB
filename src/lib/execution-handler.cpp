1883a6b323e86120509f3ae938dbf0ae72cd1d7e
Cameron McConnell <cameron@angry-dogman.com>
2024-07-07 09:38:25 -0500

    Defined select function for ExecutionHandler class.
    Gonna need to do a lot of refactoring for this code. I need to do something
    to get the output to the socket class so the program can respond to the connected
    user.

--- src/lib/execution-handler.cpp
+++ src/lib/execution-handler.cpp
@@ -15,7 +15,7 @@ void ExecutionHandler::create(std::string tableName, std::vector<std::string> he
 }
 
 void ExecutionHandler::drop(std::string tableName) {
-    
+
 }
 
 void ExecutionHandler::use(std::string tableName) {
@@ -40,15 +40,52 @@ void ExecutionHandler::insert(std::unordered_map<std::string, std::string> state
 }
 
 void ExecutionHandler::select(std::unordered_map<std::string, Predicate> conditions) {
-    
+    std::unordered_map<std::string, std::string> verifyRow = this->table[0];
+
+    for (auto columnValuePair : conditions) {
+        std::string header = columnValuePair.first;
+        if (verifyRow.find(header) == verifyRow.end()) {
+            std::string message = "SYNTAX ERROR; HEADER " + columnValuePair.first + " IS NOT PRESENT IN TABLE " + this->activeTable + ";";
+            throw SyntaxError(message);
+        }
+    }
+
+    std::vector<std::unordered_map<std::string, std::string>> selectRows;
+
+    for (auto row : this->table) {
+        for (auto columnValuePair : row) {
+            std::string header = columnValuePair.first;
+            
+            if (conditions.find(header) != conditions.end()) {
+                Operator op = conditions[header].op;
+                std::string conditionsValue = conditions[header].value;
+                std::string tableValue = row[header];
+                
+                if (op == Operator::EQUAL && tableValue == conditionsValue)
+                    selectRows.push_back(row);
+                else if (op == Operator::NOT_EQUAL && tableValue != conditionsValue)
+                    selectRows.push_back(row);
+                else if (op == Operator::LESS_THAN && tableValue < conditionsValue)
+                    selectRows.push_back(row);
+                else if (op == Operator::GREATER_THAN && tableValue > conditionsValue)
+                    selectRows.push_back(row);
+                else if (op == Operator::LESS_EQUAL && tableValue <= conditionsValue)
+                    selectRows.push_back(row);
+                else if (op == Operator::GREATER_EQUAL && tableValue >= conditionsValue)
+                    selectRows.push_back(row);
+            }
+        }
+    }
+
+    std::string csvString = csvParser.toCsvString(selectRows);
 }
 
 void ExecutionHandler::selectAll() {
-    
+    std::string csvString = csvParser.toCsvString(this->table);
 }
 
 void ExecutionHandler::delete_(std::unordered_map<std::string, Predicate> condtions) {
-
+    
 }
 
 void ExecutionHandler::update(std::unordered_map<std::string, Predicate> conditions, std::unordered_map<std::string, std::string> statements) {
